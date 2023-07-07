288534f6a3da9f125a5110ec677d605bbf9033f1
김명식 <rlaaudtlr233@naver.com>
2023-07-07 17:50:00 +0900

    [feat] 라우터 설정 및 라우팅 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/nevbar/Navbar';
 
 function App() {
-  return <div>faefa</div>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
