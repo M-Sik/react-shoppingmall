16d38823d4885d6414a4229fe8709ff37081891f
김명식 <rlaaudtlr233@naver.com>
2023-07-07 18:02:01 +0900

    [design] header navbar 스타일 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts, tsx, js, jsx'],
+  content: ['./src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#f96162',
+      },
+    },
   },
   plugins: [],
 };
