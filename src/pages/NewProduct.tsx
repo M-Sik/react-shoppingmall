f4598b3288876183112981d3e020a802e055915c
김명식 <rlaaudtlr233@naver.com>
2023-07-14 17:02:35 +0900

    [feat] 제품 등록페이지 입력 폼 및 이미지 미리보기 구현

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,5 +1,85 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+
+type Product = {
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  options: string;
+};
 
 export default function NewProduct() {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState<Product>({
+    title: '',
+    price: '',
+    category: '',
+    description: '',
+    options: '',
+  });
+  const [file, setFile] = useState<File | null>();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // 1. 제품의 사진을 cloudinary에 업로드 후 url 획득
+    // 2. firebase에 새로운 제품을 추가
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept="image/*" name="file" required onChange={handleChange} />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ''}
+          placeholder="제품명"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ''}
+          placeholder="가격"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="price"
+          value={product.category ?? ''}
+          placeholder="카테고리"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.description ?? ''}
+          placeholder="제품 설명"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="options"
+          value={product.options ?? ''}
+          placeholder="옵션들(콤마(,)로 구분)"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" className="bg-brand text-white py-2 px-4">
+          제품 등록하기
+        </button>
+      </form>
+    </section>
+  );
 }
