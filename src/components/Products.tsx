f56e7def1c944697e8c79d883bfca4991214fe1c
김명식 <rlaaudtlr233@naver.com>
2023-07-16 13:50:02 +0900

    [design] Products 페이지 스타일링

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -11,7 +11,7 @@ export default function Products() {
     <>
       {isLoading && <p>Loading....</p>}
       {error && <p>error</p>}
-      <ul>
+      <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {products &&
           products.map((product: any) => <ProductCard key={product.id} product={product} />)}
       </ul>
--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -7,13 +7,13 @@ type Props = {
 
 export default function ProductCard({ product: { id, image, title, category, price } }: Props) {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <h3>{title}</h3>
-        <p>{`${price}원`}</p>
+    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+      <img className="w-full" src={image} alt={title} />
+      <div className="mt-2 px-2 text-lg">
+        <h3 className="font-bold truncate">{title}</h3>
+        <p className="text-brand font-semibold">{`${price}원`}</p>
       </div>
-      <p>{category}</p>
+      <p className="my-2 px-2 text-gray-500">{category}</p>
     </li>
   );
 }
